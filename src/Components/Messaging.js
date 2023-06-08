7f140cadb10811d74a2515b3ff42535c00ddbad9
sateeshkostu <sateeshkostu45@gmail.com>
2023-06-08 14:34:05 +0530

    communication message

--- src/Components/Messaging.js
+++ src/Components/Messaging.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import axios from "axios";
+import Card from 'react-bootstrap/Card';
 export default function Messaging() {
 
     const [show, setShow] = useState(false);
@@ -10,9 +11,11 @@ export default function Messaging() {
     const [desc, setDesc] = useState()
     const [title, setTitle] = useState()
     const [userType, setUserType] = useState([])
+    const [notification, setNotification] = useState([])
 
-
-
+    useEffect(() => {
+        getnotification()
+    }, [])
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -95,7 +98,7 @@ export default function Messaging() {
             description: desc,
             externalId: userType
         }
-        const result = await axios.post("http://localhost:3001/admin/sendMessages", body)
+        const result = await axios.post("https://motionless-cowboy-hat-ant.cyclic.app/admin/sendMessages", body)
         try {
             if (result) {
                 console.log(result.data.message)
@@ -105,68 +108,104 @@ export default function Messaging() {
             console.log("Error")
         }
     }
+    
+    const getnotification = async () => {
+        await axios
+            .get('https://motionless-cowboy-hat-ant.cyclic.app/admin/allNotifications')
+            .then(response => {
+                const result = response.data.notifications
+                setNotification(result)
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
 
-    console.log(message)
+    console.log(notification)
     return (
-        <div style={{ marginTop: '10px', textAlign: 'right' }}>
-            {/* <button > CreateMessage</button> */}
-            <Button onClick={handleShow} variant="light" style={{ backgroundColor: "#F58E26", color: "white", margin: '2rem' }}>
-                Create Message
-            </Button>
+        <>
+            <div style={{ marginTop: '10px', textAlign: 'right' }}>
+                {/* <button > CreateMessage</button> */}
+                <Button onClick={handleShow} variant="light" style={{ backgroundColor: "#F58E26", color: "white", margin: '2rem' }}>
+                    Create Message
+                </Button>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Create Message</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create Message</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
 
-                            <Form.Control
-                                type="text"
-                                placeholder="Title"
-                                autoFocus
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Title"
+                                    autoFocus
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlTextarea1"
+                            >
 
-                            <Form.Control as="textarea" value={desc} onChange={(e) => setDesc(e.target.value)} rows={3} placeholder="Description/Message" />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
+                                <Form.Control as="textarea" value={desc} onChange={(e) => setDesc(e.target.value)} rows={3} placeholder="Description/Message" />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlTextarea1"
+                            >
 
-                            <select style={{ width: '30.5vw', height: "40px", borderRadius: '10px' }} onChange={(e) => getdata(e.target.value)} >
-                                <option selected> Send to</option>
-                                <option value="All"> All</option>
-                                <option value="Shippers">Shippers</option>
-                                <option value="Transporters">Transporters</option>
-                                <option value="Agents">Agents</option>
-                                <option value="FleetOwner">FleetOwner</option>
+                                <select style={{ width: '30.5vw', height: "40px", borderRadius: '10px' }} onChange={(e) => getdata(e.target.value)} >
+                                    <option selected> Send to</option>
+                                    <option value="All"> All</option>
+                                    <option value="Shippers">Shippers</option>
+                                    <option value="Transporters">Transporters</option>
+                                    <option value="Agents">Agents</option>
+                                    <option value="FleetOwner">FleetOwner</option>
 
-                            </select>
-                            <h6 style={{ marginTop: '10px' }}>Notification/Frequency</h6>
-                            <Form.Control type="date" style={{ padding: '10px', width: '150px' }} />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                {/* <Modal.Footer style={{marginRight:'130px'}}> */}
-                <div style={{ display: 'flex', alignItems: 'center', marginLeft: '190px', marginBottom: '20px' }}>
-                    <Button style={{ backgroundColor: '#FFF', color: 'black', marginRight: '30px' }} onClick={handleClose}>
-                        Cancel
-                    </Button>
-                    <Button style={{ backgroundColor: '#F58E26' }} onClick={handleMessage}>
-                        Send
-                    </Button>
-                </div>
-                {/* </Modal.Footer> */}
-            </Modal>
-        </div>
+                                </select>
+                                <h6 style={{ marginTop: '10px' }}>Notification/Frequency</h6>
+                                <Form.Control type="date" style={{ padding: '10px', width: '150px' }} />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    {/* <Modal.Footer style={{marginRight:'130px'}}> */}
+                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '190px', marginBottom: '20px' }}>
+                        <Button style={{ backgroundColor: '#FFF', color: 'black', marginRight: '30px' }} onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button style={{ backgroundColor: '#F58E26' }} onClick={handleMessage}>
+                            Send
+                        </Button>
+                    </div>
+                    {/* </Modal.Footer> */}
+                </Modal>
+            </div>
+            <div style={{}}>
+                
+                    
+                        {notification.map((item, index) => {
+                            return (
+                                <Card className="container" style={{marginBottom:'20px', width:'800px'}}>
+                                    <h4 style={{ color: "#F58E26", marginTop:'10px' }}>Truk App</h4>
+                                    <p style={{fontWeight:'bold'}}>To:{item.to}</p>
+                                    <p style={{fontWeight:'bold'}}>{item.title}</p>
+                                    <p style={{fontWeight:'bold'}}>{item.description}</p>
+                                    <h7 style={{ textAlign: 'right', fontWeight:'bold' }}>{item.time}</h7>
+                                </Card>
+
+                            )
+
+                        })}
+                        {/* <p>title</p>
+                        <p>description</p>
+                        <h6 style={{ textAlign: 'right' }}>time</h6> */}
+                    
+                
+            </div>
+        </>
     )
 }
