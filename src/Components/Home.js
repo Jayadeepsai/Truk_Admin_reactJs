a3375de569ca29ea678baf5866aca22fcc82aebe
sateeshkostu <sateeshkostu45@gmail.com>
2023-06-15 12:21:39 +0530

    satttiiiiiiiiiii

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -118,40 +118,43 @@ export default function Home(props) {
     return (
         <>
             <br />
-            <h2 style={{ textAlign: 'center', fontSize: '40px' }}>Dashboard</h2>
-            <div style={{ display: 'flex', marginBottom: '30px', }}>
-                <Card style={{ width: '350px', height: '200px', left: '400px', top: '80px', borderColor: '#F58E26', }} onClick={shippersCount}>
-                    <Card.Body>
-                        <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Shippers</Card.Title>
-                        <p style={{ marginLeft: '70px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{shipperCount}</b></p>
-                        {/* {shippers.map(count=>(<>
+            <div className='parent container' style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <h2 style={{ textAlign: 'center', fontSize: '40px' }}>Dashboard</h2>
+                <div className='container' style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <div style={{ display: 'flex', marginBottom: '30px', }}>
+                        <Card style={{ width: '350px', height: '200px', top: '80px', borderColor: '#F58E26', }} onClick={shippersCount}>
+                            <Card.Body>
+                                <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Shippers</Card.Title>
+                                <p style={{ marginLeft: '70px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{shipperCount}</b></p>
+                                {/* {shippers.map(count=>(<>
                         <p>Count:{count.users}</p>
                         </> ))} */}
 
 
-                    </Card.Body>
-                </Card>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <Card style={{ width: '350px', height: '200px', left: '483px', top: '80px', borderColor: '#F58E26' }} onClick={AgentsCount}>
-                    <Card.Body>
-                        <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Agents</Card.Title>
-                        <p style={{ marginLeft: '80px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{agentsCount}</b></p>
-                    </Card.Body>
-                </Card>
-            </div>
-
-            <div style={{ display: 'flex', }}>
-                <Card style={{ width: '350px', height: '200px', left: '400px', top: '100px', borderColor: '#F58E26' }} onClick={transportersCount}>
-                    <Card.Body>
-                        <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Transporters</Card.Title>
-                        <p style={{ marginLeft: '53px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{transporterCount}</b></p>
-                    </Card.Body>
-                </Card>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <Card style={{ width: '350px', height: '200px', left: '483px', top: '100px', borderColor: '#F58E26' }} onClick={fleetsCount}>
-                    <Card.Body>
-                        <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Fleet Owners</Card.Title>
-                        <p style={{ marginLeft: '53px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{fleetOwnersCount}</b></p>
-                    </Card.Body>
-                </Card>
+                            </Card.Body>
+                        </Card>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Card style={{ width: '350px', height: '200px',  top: '80px', borderColor: '#F58E26' }} onClick={AgentsCount}>
+                            <Card.Body>
+                                <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Agents</Card.Title>
+                                <p style={{ marginLeft: '80px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{agentsCount}</b></p>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div style={{ display: 'flex', }}>
+                        <Card style={{ width: '350px', height: '200px',  top: '100px', borderColor: '#F58E26' }} onClick={transportersCount}>
+                            <Card.Body>
+                                <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Transporters</Card.Title>
+                                <p style={{ marginLeft: '53px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{transporterCount}</b></p>
+                            </Card.Body>
+                        </Card>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Card style={{ width: '350px', height: '200px', top: '100px', borderColor: '#F58E26' }} onClick={fleetsCount}>
+                            <Card.Body>
+                                <Card.Title style={{ textAlign: 'center', marginTop: '50px', marginRight: '50px' }}>Total Fleet Owners</Card.Title>
+                                <p style={{ marginLeft: '53px' }}>Count:&nbsp;&nbsp;&nbsp;<b>{fleetOwnersCount}</b></p>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
             </div>
         </>
     )
