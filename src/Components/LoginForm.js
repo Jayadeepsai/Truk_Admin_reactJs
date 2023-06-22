c193174bb9796d7bc8011319eba2cf996c8667e4
sateeshkostu <sateeshkostu45@gmail.com>
2023-06-22 15:17:33 +0530

    validations

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
@@ -7,136 +7,119 @@ import { InputGroup } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import axios from "axios";
 
-
-
 const LoginForm = (props) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-    const PasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
+  const PasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    // Perform validation
+    if (password === '') {
+      setPasswordError('Password is required');
+    } else {
+      setPasswordError('');
+    }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    if (email === '') {
+      setEmailError('Email is required');
+    } else {
+      setEmailError('');
+    }
 
-        // Send login data to the server here
-        const body = {
-            email: email,
-            password: password
+    // Continue with form submission if no errors
+    if (password !== '' && email !== '') {
+      // Send login data to the server here
+      const body = {
+        email: email,
+        password: password
+      };
+
+      try {
+        const response = await axios.post("https://motionless-cowboy-hat-ant.cyclic.app/admin/loginAdmin", body);
+        console.log(response);
+        if (response.status === 200) {
+          alert('Login successful');
+          localStorage.setItem('adminEmail', JSON.stringify(response.data.data));
+          localStorage.setItem('isLoggedIn', true);
+          navigate('/home');
+        } else {
+          alert('Please check your credentials');
         }
-        await axios.post("https://motionless-cowboy-hat-ant.cyclic.app/admin/loginAdmin", body)
-            .then((response) => {
-                console.log(response);
-                if (response.status === 200) {
-                    alert('Login succesfull')
-                    localStorage.setItem('adminEmail', JSON.stringify(response.data.data));
-                    localStorage.setItem('isLoggedIn', true)
-                    navigate('/home');
-                } else {
-                    alert('Please check your credentials')
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-                console.log("Login Failed")
-                alert('login failed')
-            });
-        // console.log("Email:", email);
-        // console.log("Password:", password);
-        // Redirect to home page after successful login
-    };
-    return (
-        <>
-            <div className="trukapp" style={{ display: 'flex' }}>
+      } catch (error) {
+        console.log(error);
+        console.log("Login Failed");
+        alert('Login failed');
+      }
+    }
+  };
 
-
-                <div style={{ textAlign: 'center', width: '50%', height: '100%' }}>
-                    <div style={{ fontSize: '100px' }}>
-                        <img src="http://trukapp.com/wp-content/uploads/2021/11/1599843225043_truckapp-5.svg" width='200px' height='200px' />
-                    </div>
-                    <div className="login"  >
-                        <div style={{ textAlign: 'center', fontSize: '20px' }}>
-                            <h3>Login to your account</h3>
-                            <form style={{ textAlign: 'center', fontSize: '30px' }}>
-                                <div className="field">
-                                    <div>
-                                        {/* <label >Email    :  </label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
-                                        {/* <input
-                                            type="email"
-                                            placeholder="Email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                        /> */}
-                                        <InputGroup className="mb-3" style={{ width: '30rem', margin: 'auto' }}>
-                                            <InputGroup.Text id="basic-addon1" style={{ width: "6rem", alignItems: 'center' }}>Email</InputGroup.Text>
-                                            <Form.Control
-                                                type="email"
-                                                placeholder="Email"
-                                                aria-label="Username"
-                                                aria-describedby="basic-addon1"
-                                                value={email}
-                                                onChange={(e) => setEmail(e.target.value)}
-                                            />
-                                        </InputGroup>
-                                    </div><br />
-                                    <div style={{ display: 'flex' }}>
-
-                                        {/* <InputGroup>
-                                        <input
-                                            type={showPassword ? 'text' : 'password'}
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            
-                                        />
-                                        <span onClick={PasswordVisibility}>
-                                            {showPassword ? <FaEye /> : <FaEyeSlash />}
-                                        </span>
-                                        </InputGroup> */}
-                                        <InputGroup className="mb-3" style={{ width: '30rem', margin: 'auto' }}>
-                                            <InputGroup.Text id="basic-addon1" style={{ width: "6rem", textAlign: 'center' }}>Password</InputGroup.Text>
-                                            <Form.Control
-
-                                                type={showPassword ? 'text' : 'password'}
-                                                placeholder="Password"
-                                                value={password}
-                                                onChange={(e) => setPassword(e.target.value)}
-                                            />
-                                            <InputGroup.Text onClick={PasswordVisibility}>
-                                                {showPassword ? <FaEye /> : <FaEyeSlash />}</InputGroup.Text>
-                                        </InputGroup>
-                                    </div>
-                                    {/* <div>
-                                        <a href="" style={{fontSize:"1rem"}}>Forget password?</a>
-                                    </div> */}
-                                </div>
-
-                                {/* <div className="radi">
-                                        <input type="radio" />&nbsp;
-                                        <a href="">Remember me</a>
-                                    </div> */}
-
-
-
-                                <div >
-                                    <Button className="button" variant='light' style={{ backgroundColor: '#F58E26', marginLeft: '20px', color: "white" }} onClick={(e) => handleSubmit(e)}>Login now</Button>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="trukapp" style={{ display: 'flex' }}>
+        <div style={{ textAlign: 'center', width: '50%', height: '100%' }}>
+          <div style={{ fontSize: '100px' }}>
+            <img src="http://trukapp.com/wp-content/uploads/2021/11/1599843225043_truckapp-5.svg" width='200px' height='200px' />
+          </div>
+          <div className="login"  >
+            <div style={{ textAlign: 'center', fontSize: '20px' }}>
+              <h3>Login to your account</h3>
+              <form style={{ textAlign: 'center', fontSize: '30px' }}>
+                <div className="field">
+                  <div>
+                    <InputGroup className="mb-3" style={{ width: '30rem', margin: 'auto' }}>
+                      <InputGroup.Text id="basic-addon1" style={{ width: "6rem", alignItems: 'center' }}>Email</InputGroup.Text>
+                      <Form.Control
+                        type="email"
+                        placeholder="Email"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                     
+                    </InputGroup>
+                    {emailError && <p className="error" style={{color:'red', fontSize:'15px'}}>{emailError}</p>}
+                  </div>
+                  <br />
+                  <div>
+                    <InputGroup className="mb-3" style={{ width: '30rem', margin: 'auto' }}>
+                      <InputGroup.Text id="basic-addon1" style={{ width: "6rem", textAlign: 'center' }}>Password</InputGroup.Text>
+                      <Form.Control
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <InputGroup.Text onClick={PasswordVisibility}>
+                        {showPassword ? <FaEye /> : <FaEyeSlash />}
+                      </InputGroup.Text>
+                     
+                    </InputGroup>
+                    {passwordError && <p className="error" style={{color:'red',fontSize:'15px'}}>{passwordError}</p>}
+                  </div>
                 </div>
-                <div className="img" style={{ width: '50%', height: '100vh' }}>
-                    <img src="https://static1.squarespace.com/static/55d64111e4b0a862eed6a419/55d80b75e4b0c9ab9657c9c8/62186795c1b2863807ba81d8/1645808539405/unsplash-image-3jG-UM8IZ40.jpg?format=1500w" style={{ maxWidth: '100%', maxHeight: '100vh' , height:'750px'}} />
+                <div>
+                  <Button className="button" variant='light' style={{ backgroundColor: '#F58E26', marginLeft: '20px', color: "white" }} onClick={(e) => handleSubmit(e)}>Login now</Button>
                 </div>
-
+              </form>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+        <div className="img" style={{ width: '50%', height: '100vh' }}>
+          <img src="https://static1.squarespace.com/static/55d64111e4b0a862eed6a419/55d80b75e4b0c9ab9657c9c8/62186795c1b2863807ba81d8/1645808539405/unsplash-image-3jG-UM8IZ40.jpg?format=1500w" style={{ maxWidth: '100%', maxHeight: '100vh', height: '750px' }} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default LoginForm;
+export default LoginForm
